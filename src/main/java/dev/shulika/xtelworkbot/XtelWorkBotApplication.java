67475c29639361985b7612d29a3a5149bb34d2c9
Sergey Shulika <developer.shulika@gmail.com>
2023-04-21 17:58:12 +0300

    init commit

---
+++ src/main/java/dev/shulika/xtelworkbot/XtelWorkBotApplication.java
@@ -0,0 +1,13 @@
+package dev.shulika.xtelworkbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XtelWorkBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XtelWorkBotApplication.class, args);
+    }
+
+}
